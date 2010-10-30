7135744b95117684dcd250daaee6058f3e6130e2
ciampagg <ciampagg@usi.ch>
2010-10-30 10:48:45 -0700

    ADD cpeers.py
    Ignore-this: 62bc67233da7fc40dd499431c023abfb
    
    darcs-hash:20101030174845-38e5d-ec012e1653f0b12dc75598990eddff7a0b717479.gz

---
+++ cpeers.py
@@ -0,0 +1,32 @@
+''' fast Cython implementation '''
+
+# will import only __all__
+from peers import *
+from _cpeers import simulate 
+import sys
+
+if __name__ == '__main__':
+    parser = make_parser()
+    ns = parser.parse_args()
+    try:
+        check_arguments(ns)
+        print_arguments(ns)
+        if not ns.dry_run:
+            if ns.profile:
+                import pstats, cProfile
+                fn = ns.profile_file or __file__ + ".prof"
+                cProfile.runctx('simulate(ns)', globals(), locals(), fn)
+                stats = pstats.Stats(fn)
+                stats.strip_dirs().sort_stats("time").print_stats()
+            else:
+                prng, users, pages = simulate(ns)
+    except:
+        ty, val, tb = sys.exc_info()
+        if ns.debug:
+            raise ty, val, tb
+        else:
+            name = ty.__name__
+            print >> sys.stderr, '\n%s: %s\n' % (name, val)
+    finally:
+        if ns.info_file:
+            ns.info_file.close()
