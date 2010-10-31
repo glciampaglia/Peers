33604cdd473c40832d96a647615a9a757e8dae91
ciampagg <ciampagg@usi.ch>
2010-10-31 15:11:14 -0700

    ADD sim.sh
    Ignore-this: 3787efc3dde6406cf477e6903625dea0
    
    darcs-hash:20101031221114-38e5d-8687707d8a0bc0643db2e1f8dbaf016260489d98.gz

---
+++ sim.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+num=10
+file=prova
+python jobs.py 'python cpeers.py' -p 'python lt.py' -s defaults\
+    -S ltdefaults -d confidence -d rollback-prob -r 0 1 -r 0 1 -o $file lhd $num\
+    | python cluster.py -v
+
+if [ "$?" = 0 ];
+then
+    zip $file.npz $file-*.npy $file_index.npy && rm -f $file-*npy $file_index.npy
+else
+    echo 'failed'
+fi
