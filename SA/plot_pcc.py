997104459b9cf0fc7df3f9f9adcfb88f7b78cd68
ciampagg <ciampagg@usi.ch>
2011-01-31 09:44:26 -0800

    ADD partial correlation coefficient computation
    Ignore-this: 70d0e2213b686db2b0819d235e6531e9
    
    darcs-hash:20110131174426-38e5d-bae1b1c87ec356253639a5f5e2a99e70a685d2bc.gz

--- SA/plot_pcc.py
+++ SA/plot_pcc.py
@@ -1,7 +1,19 @@
 from __future__ import division
 from argparse import ArgumentParser, FileType
-import matplotlib.pyplot as pp
 import numpy as np
+import matplotlib.pyplot as pp
+
+def pcc(x,y,z):
+    '''
+    x,y - (N,) arrays of variables
+    z   - (M,N) array of M confounding variables
+
+    Computes the partial correlation coefficient between x and y, given z
+    '''
+    zorth = np.linalg.qr(z.T)[0].T
+    x = x - np.dot(np.dot(zorth, x), zorth)
+    y = y - np.dot(np.dot(zorth, y), zorth)
+    return np.dot(x,y)/np.sqrt(np.dot(x,x) * np.dot(y,y))
 
 def main(args):
     data = np.loadtxt(args.data, delimiter=args.delimiter)
@@ -35,11 +47,15 @@ def main(args):
         for i in xrange(d):
             ax = fig.axes[i]
             ax.set_title(args.params[i].replace('_',' ').capitalize())
-            print '%s,%g' % (args.params[i], np.corrcoef(X[:,i],y)[0,1])
+            idx = range(d)
+            del idx[i]
+            print '%s,%g' % (args.params[i], pcc(X[:,i], y, X[:,idx].T))
     else:
         for i in xrange(d):
-            print np.corrcoef(X[:,i],y)[0,1]
-    # set ticks on x/y axis
+            idx = range(d)
+            del idx[i]
+            print pcc(X[:,i],y,z[:,idx].T)
+    # set ticks on x/y axis, y label only on first plot
     for i in xrange(d):
         ax = fig.axes[i]
         xmin, xmax = ax.get_xlim()
@@ -51,20 +67,29 @@ def main(args):
         if i == 0:
             ax.set_ylabel(r'$<\tau>$ (days)',fontsize=14)
     if args.output is not None:
-        pp.savefig(args.output)
+        for out in args.output:
+            pp.savefig(out)
     pp.show()
 
-if __name__ == '__main__':
+def make_parser():
     parser = ArgumentParser(description='plots scatters of data on parameter '\
             'values.')
-    parser.add_argument('data', type=FileType('r'))
+    parser.add_argument('data', type=FileType('r'), metavar='FILE', help='data'\
+            ' file')
     parser.add_argument('-p', '--parameters', type=FileType('r'), help='set '\
-            'titles to parameter names', dest='params_file')
-    parser.add_argument('-o', '--output', type=FileType('w'), help='output file')
+            'titles to parameters taken from %(metavar)s', dest='params_file', 
+            metavar='FILE')
+    parser.add_argument('-o', '--output', type=FileType('w'), help='save '\
+            'graphics in %(metavar)s', nargs='+', metavar='FILE')
     parser.add_argument('-d', '--delimiter', default=',', help='delimiter of '\
-            'data values')
+            'data values', metavar='CHAR')
     parser.add_argument('-e','--error-bars', action='store_true', help='plot '\
-            'error bars')
-    parser.add_argument('-c','--columns', type=int, default=3, dest='cols')
+            'error bars (standard errors in last column)')
+    parser.add_argument('-c','--columns', type=int, default=3, dest='cols',
+            help='arrange plots in %(metavar)s columns', metavar='NUM')
+    return parser
+
+if __name__ == '__main__':
+    parser = make_parser()
     ns = parser.parse_args()
     main(ns)
