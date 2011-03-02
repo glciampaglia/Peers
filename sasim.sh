61926903058be62530bcefff859f23f60e5a16aa
ciampagg <ciampagg@usi.ch>
2011-03-02 06:32:50 -0800

    Added parameter \Delta t for simulations in sasim.sh
    Ignore-this: a219b2e20e783f23427cf87de6bdf2fd
    
    darcs-hash:20110302143250-38e5d-ad60524b5104573470ac76315f6b2f66903cae46.gz

--- sasim.sh
+++ sasim.sh
@@ -10,16 +10,20 @@ order=(daily_edits daily_users daily_pages confidence speed const_succ
 
 echo ${order[@]} | sed -e 's/ /,/g' > params.txt
 
-# python lhd.py -i 1 100 -i 1 200 -i 1 200 -i 0 1 -i 0 0.5 -i 0 100 -i 0 100\
-#     -i 0 1 $size 8 > sample.txt
+python lhd.py -i 1 100 -i 1 200 -i 1 200 -i 0 1 -i 0 0.5 -i 0 100 -i 0 100\
+     -i 0 1 -i 0 7 $size 9 > sample.txt
 
-python winding.py -i 1 100 -i 1 200 -i 1 200 -i 0 1 -i 0 0.5 -i 0 100 -i 0 100\
-    -i 0 1 $size 8 > sample.txt
-size=$(($size*${#order[@]})) # needed for the winding sampling
+# python winding.py -i 1 100 -i 1 200 -i 1 200 -i 0 1 -i 0 0.5 -i 0 100 -i 0 100\
+#     -i 0 1 -i 0 7 $size 9 > sample.txt
+# size=$(($size*${#order[@]})) # needed for the winding sampling
 
-options="-e %(e)g -U %(U)g -P %(P)g -c %(c)g -s %(s)g --const-succ %(cs)g --const-pop %(cp)g --rollback-prob %(rp)g"
-step=$(echo 1/24|bc -l)
-sim_cmd="python cpeers.py $options -T 1 -t $step 1"
+options="-e %(e)g -U %(U)g -P %(P)g -c %(c)g -s %(s)g --const-succ %(cs)g --const-pop %(cp)g --rollback-prob %(rp)g -t %(s)g"
+
+# this is for a fixed \Delta t
+# step=$(echo 1/24|bc -l)
+# sim_cmd="python cpeers.py $options -T 1 -t $step 1" 
+
+sim_cmd="python cpeers.py $options -T 1 100"
 lt_cmd="python lt.py -l out_%(count)s.npy"
 ind_cmd="echo out_%(count)s.npy >> /tmp/index.txt"
 
