8813d1466abe763666284111198d03c8991ba6ec
ciampagg <ciampagg@usi.ch>
2011-03-03 03:14:27 -0800

    added fitsim.sh
    Ignore-this: 8fe94b9ce5c1f899a91071a3ad983525
    
    darcs-hash:20110303111427-38e5d-b9c24af04a256771fb812d6c617243ecc8a80946.gz

---
+++ fitsim.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# fitsim.sh - simulation for fitting model
+
+# Simulate each value $reps time, for all $size values.
+size=25 # sample size
+reps=10 # repetitions
+
+# Fix model parameters
+baselife=100 # in days
+dailyusers=10 # users/day
+dailypages=1 # pages/day
+dailyedits=5 # edits/day
+timestep=$(echo 1/24|bc -l) # in days
+simtime=365 # in days
+trantime=7300 # in days
+cat > defaults <<EOF
+-b
+$baselife
+-U
+$dailyusers
+-P
+$dailypages
+-e
+$dailyedits
+-t
+$timestep
+-T
+$trantime
+$simtime
+EOF
+
+# Define input sample
+order=(confidence)
+echo ${order[@]} | sed -e 's/ /,/g' > params.txt
+step=$(echo 1/$size|bc -l)
+LC_ALL=en seq $step $step 1 > sample.txt # for English locale for decimal sep
+
+# Define simulator commands
+sim_cmd="python cpeers.py -e %(e)g @defaults"
+lt_cmd="python lt.py -lL out_%(count)s.npy" # store log-lifetime
+ind_cmd="echo out_%(count)s.npy >> /tmp/index.txt"
+
+trap 
+
+# Launch simulation
+# ipcluster ssh --clusterfile cluster_conf.py 2>&1 >cluster.log &
+ipcluster local -n 2 2>&1 >cluster.log &
+PID=$!
+echo -n "Waiting for ipcluster($PID) to start..."
+sleep 5
+python jobs.py -r $reps "$sim_cmd | $lt_cmd" < sample.txt | python pexec.py -v
+kill -2 $PID
+sleep 2
+kill -CONT $PID &>/dev/null # if kill returns 0 then process is still alive
+if [[ $? = 0 ]]
+then
+    echo "ipcluster($PID) did not terminate. Stop it manually."
+fi
+
+# Create the index of output files
+seq 0 $(($size*$reps-1)) | sed -e's/.*/out_&.npy/' > /tmp/index.txt
+rep_script=$(cat <<EOF
+import sys
+for l in sys.stdin.readlines():
+    for i in xrange($reps):
+        print l,
+EOF
+)
+python -c "$rep_script" < sample.txt > /tmp/sample.txt
+paste -d, /tmp/sample.txt /tmp/index.txt > index.txt
+rm -f /tmp/{index,sample}.txt
+
+# Compress all output files plus index, sample, defaults and parameters list 
+tar cvfz out.tar.gz out*npy sample.txt index.txt params.txt defaults &>/dev/null
+TAR_ESTAT=$?
+if [[ $TAR_ESTAT = 0 ]]
+then
+    echo "All files compressed. Removing intermediate files."
+    rm -f out*.npy {sample,index,params}.txt
+    echo "Simulation output stored in out.tar.gz."
+else
+    echo "Error: tar exited with status $TAR_ESTAT."
+    echo "No intermediate file has been removed."
+fi
+
