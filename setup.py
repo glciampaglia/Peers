837e81a2a90d9c27845d47bbe0dc7f2c2d88427d
ciampagg <ciampagg@usi.ch>
2011-03-27 09:30:08 -0700

    moved peers/setup.py in top-level folder (as setup_cython.py now) and cleaned up code + FIX author_email keyword in call to setup in setup.py
    Ignore-this: aca2856079d88d1f2acbc8bde108bf12
    
    darcs-hash:20110327163008-38e5d-f64425941ae9455d5be4bf3c2666477473ad92d0.gz

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from numpy import get_include
+
+_I = [ get_include(), 'peers']
 
 setup(
         name='peers',
         description='An agent-based simulator for the Peers model.',
         version=0.0,
         author='Giovanni Luca Ciampaglia',
-        author_mail='ciampagg@usi.ch',
+        author_email='ciampagg@usi.ch',
         packages=[
             'peers', 
             'peers.gsa', 
@@ -13,5 +17,9 @@ setup(
             'peers.mde', 
             'peers.tests',
             'peers.design',
+        ],
+        ext_modules = [
+            Extension("peers.rand", ["peers/rand.c"], include_dirs=_I), 
+            Extension("peers.cpeers", ["peers/cpeers.c"], include_dirs=_I) 
         ]
 )
---
+++ setup_cython.py
@@ -0,0 +1,38 @@
+''' 
+Development-only distutils setup script. Intended for use with command build_ext
+(possibly with --inplace
+'''
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from numpy import get_include
+
+# C includes
+_I = [ get_include(), 'peers']
+
+setup(
+        cmdclass = { 'build_ext' : build_ext },
+        ext_modules = [
+            Extension("peers.rand", ["peers/rand.pyx"], include_dirs=_I), 
+            Extension("peers.cpeers", ["peers/cpeers.pyx"], include_dirs=_I) 
+        ]
+)
+
+# OLD: compile an extension that makes direct calls to randomkit in NumPy. This
+# is supposed to require a copy of Python.pxi in the source tree.
+
+# # Get the include path for NumPy's randomkit extension module.
+# import numpy as np
+# from os.path import dirname, join
+# numpy_root = dirname(np.__file__)
+# numpy_random_include = join(numpy_root, 'random')
+# numpy_mtrand_include = join(numpy_random_include, 'mtrand')
+# 
+# # Put this in the list passed to argument ext_modules of setup.
+# ext_state = Extension('_state',
+#         [ '_state.pyx', 'randomkit.c' ],
+#         depends=[ 'randomkit.h', 'Python.pxi' ],
+#         include_dirs=[ numpy_include, numpy_random_include ],
+#         extra_compile_args=['-Winline',])
+
