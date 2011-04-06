a75ae970c803e08d123792bd8478902765dd0c4b
ciampagg <ciampagg@usi.ch>
2011-04-06 07:14:05 -0700

    added simulation script for transient
    Ignore-this: 209158d02f01ea721432f073bbfcc27b
    
    darcs-hash:20110406141405-38e5d-e21e60429ecbec52a57526716982d9659b027aae.gz

---
+++ scripts/transim.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# transim.sh - simulation for transient time estimation
+
+# Simulate each value $reps time, for all $size values.
+size=2 # sample size
+reps=1 # repetitions
+
+# Fix model parameters
+shortlife=$(echo 1/24|bc -l) # in days
+longlife=100 # in days
+dailyusers=50 # users/day
+dailypages=10 # pages/day
+dailyedits=5 # edits/day
+simtime=5 # in days
+trantime=5 # in days
+cat > defaults <<EOF
+-l
+$shortlife
+-L
+$longlife
+-U
+$dailyusers
+-P
+$dailypages
+-e
+$dailyedits
+-T
+$trantime
+$simtime
+EOF
+
+# Define input sample
+order=(confidence)
+echo ${order[@]} | sed -e 's/ /,/g' > params.txt
+step=$(echo 1/$size|bc -l)
+LC_ALL=en seq $step $step 1 > sample.txt # for English locale for decimal sep
+
+# Define simulator commands
+sim_cmd="peerstool peers --fast -c %(c)g @defaults"
+lt_cmd="peerstool lt -lL out_%(count)s.npy" # store log-lifetime
+ind_cmd="echo out_%(count)s.npy >> /tmp/index.txt"
+
+source functions.sh
+
+simulate "$sim_cmd | $lt_cmd" $reps 0
+makeindex $size $reps
+compress out*npy sample.txt index.txt params.txt defaults cluster.log
+
+
