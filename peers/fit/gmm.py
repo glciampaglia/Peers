494195d02165e52511d7d6f7ea2af9a375c5c4e6
ciampagg <ciampagg@usi.ch>
2011-04-06 06:51:07 -0700

    Merged peers.fit.em{fit,reduce} into peers.fit.gmm
    Ignore-this: 9ae3bc1becc246c9aad6ceea15a5bd40
    
    darcs-hash:20110406135107-38e5d-57916a024039785a2c0c2b925eb944579c65e8d7.gz

--- peers/fit/gmm.py
+++ peers/fit/gmm.py
@@ -1,28 +1,24 @@
-#!/usr/bin/python
-# encoding: utf-8
+#!/usr/bin/env python
 
-'''
-Estimates parameters of a Gaussian Mixture Model (GMM) from input files and
-plots the fit.
-'''
+'''Reduction script for Gaussian Mixture Model (GMM) parameter estimation.'''
 
-import re
-from os.path import exists, isdir, join, extsep
 import sys
-from argparse import ArgumentParser, Action, FileType
+import os.path
+from warnings import warn
+from itertools import groupby
+from argparse import ArgumentParser, FileType 
 import numpy as np
 from scikits.learn.mixture import GMM
-import matplotlib.pyplot as pp
 from scipy.stats import norm
+import matplotlib.pyplot as pp
 
-# TODO <Fri Mar 18 14:45:16 CET 2011>:
+from ..utils import CheckDirAction
+
+# TODO <Tue Apr  5 11:03:34 CEST 2011>:
 # - plot density of individual components and not of mixture
 # - add subplot like in R's histogram function with vertical lines for each
 #   observation. Colors of lines are mapped on the a scale red to blue (or
 #   whatever color is chosen for the density lines of the components.
-
-def fmt(fn):
-    return re.search('\.(\w+)$', fn).group()[1:]
 
 def gmmpdf(x, means, variances, weights):
     res = []
@@ -30,12 +26,6 @@ def gmmpdf(x, means, variances, weights):
     for m,v in zip(means, variances):
         res.append(norm.pdf(x, m, np.sqrt(v)))
     return np.sum(np.asarray(res) * weights[:,None], axis=0)
-
-class _CheckDir(Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        if not exists(values) or not isdir(values):
-            parser.error('%s is not a directory!' % values)
-        setattr(namespace, self.dest, values)
 
 def plot(fn, data, model, bins, **params):
     fig = pp.figure()
@@ -52,55 +42,73 @@ def plot(fn, data, model, bins, **params):
     if 'confidence' in params:
         title = r'%s, $\varepsilon = %g$' % (fn, params['confidence'])
     else:
-        title = fn
+        title = fn.replace('_','\_')
     pp.title(title)
     pp.draw()
-    pp.savefig(fn, format=fmt(fn))
+    pp.savefig(fn, format=os.path.splitext(fn)[1] or 'pdf')
     pp.close()
 
 def main(args):
+    sep = args.sep
     if args.names is not None:
-        names = args.names.readline().strip().split(args.sep)
+        names = args.names.readline().strip().split(sep)
     else:
         names = None
-    linesiter = ( l.strip() for l in iter(args.index.readline,'') )
-    for line in linesiter:
-        values, fn = line.split(args.sep)
+    liter = (tuple(l.strip().split(sep)) for l in iter(args.index.readline,''))
+    for values, subiter in groupby(liter, lambda k : k[:-1]):
         if names is not None:
-            paramdict = dict(names, values)
+            paramsdict = dict(zip(names, values))
         else:
-            paramdict = {}
-        d = np.load(join(args.directory,fn))
-        if args.verbose:
-            print fn,
-        if len(d):
-            gmm = GMM(args.components)
-            gmm.fit(d)
-            gfn = extsep.join(fn.split(extsep)[:-1]) + extsep + args.format
-            plot(gfn, d, gmm, args.bins, **paramdict)
-            if args.verbose:
-                print 
-                sys.stdout.flush()
-        elif args.verbose:
-            print 'no data!'
-            sys.stdout.flush()
+            paramsdict = {}
+        beta = []
+        for fn in ( s[-1] for s in subiter ):
+            d = np.load(os.path.join(args.directory, fn))
+            if args.log:
+                d = np.log(d)
+            if len(d):
+                gmm = GMM(args.components)
+                gmm.fit(d)
+                mu, si, we = map(np.ravel, 
+                        [gmm.means, np.asarray(gmm.covars), gmm.weights])
+                idx = mu.argsort()
+                if args.plot:
+                    gfn = os.path.splitext(fn)[0] + os.path.extsep + args.format
+                    plot(gfn, d, gmm, args.bins, **paramsdict)
+                beta.append(np.hstack([mu[idx], si[idx], we[idx]]))
+            else:
+                warn('fn has no data. Skipping.', category=UserWarning)
+        if len(beta) == 0:
+            warn('Skipping parameters vector: %s.' % '\n\t'.join(values),
+                    category=UserWarning)
+            continue
+        beta = np.asarray(beta).mean(axis=0)
+        out = values + tuple(beta)
+        print sep.join(['%s'] * len(out)) % out
 
-if __name__ == '__main__':
+def make_parser():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('index', type=FileType('r'), help='Data files index.')
     parser.add_argument('components', type=int, help='Number of GMM components.')
     parser.add_argument('-C', '--directory', default='.', help='Interpret '
             'file paths in index as relative to %(metavar)s.', metavar='DIR',
-            action=_CheckDir)
+            action=CheckDirAction)
+    parser.add_argument('-p', '--plot', action='store_true', help='plot ' 
+            'histograms of data with fit')
+    parser.add_argument('-l', '--log', action='store_true', help='Take logs of '
+            'data')
     parser.add_argument('-d', '--delimiter', default=',', help='Data files index'
             ' has fields separated by %(metavar)s.', dest='sep', metavar='CHAR')
     parser.add_argument('-v', '--verbose', action='store_true', help='Be '
-            'verbose.')
+            'verbose. Warn when encountering empty data files.')
     parser.add_argument('-b', '--bins', type=int, metavar='NUM', help='Number '
-            'of histogram bins.', default=10)
+            'of histogram bins to plot', default=10)
     parser.add_argument('-n', '--names', type=FileType('r'), help='Parameter '
             'names file', metavar='FILE')
     parser.add_argument('-f' ,'--format', help='graphic output format (default:'
             ' %(default)s)', default='pdf')
+    return parser
+
+if __name__ == '__main__':
+    parser = make_parser()
     ns = parser.parse_args()
     main(ns)
