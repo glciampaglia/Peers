6577c7d30edc80a30b2dd63c68c358131f7c4437
ciampagg <ciampagg@usi.ch>
2011-04-08 01:31:02 -0700

    more informative warning messages
    Ignore-this: 61ccc4e9124908823096d9096fec0ee7
    
    darcs-hash:20110408083102-38e5d-0b65c335daf393b4661b5a79260945729ce54af2.gz

--- peers/__init__.py
+++ peers/__init__.py
@@ -1,8 +1,14 @@
 # set custom warnings.formatwarning
 
-def _fmtwarn(*args):
-    msg = args[0]
-    return '* WARNING: %s\n' % msg.args[0]
+def _fmtwarn(msg, cat, fn, no, line=None):
+    import sys
+    for p in sys.path:
+        if len(p) and fn.startswith(p):
+            n = len(p) + 1
+            fn = fn[n:]
+            break
+    warning = cat.__name__.upper()
+    return '* %s: %s (in %s:%d)\n' % (warning, msg, fn, no)
 
 import warnings as _w
 _w.formatwarning = _fmtwarn
