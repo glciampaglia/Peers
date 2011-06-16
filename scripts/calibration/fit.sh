21a88a53e6a7d8c35dc5bea6fdbecd0eb4c037fe
ciampagg <ciampagg@usi.ch>
2011-06-16 06:41:48 -0700

    added option -n/--dry to script for batch calibration
    Ignore-this: 54c8849e4cb88e8e8424405fb079cbc7
    
    darcs-hash:20110616134148-38e5d-829d34e2e608f89f80f6a42f07652bd611c34c87.gz

--- scripts/calibration/fit.sh
+++ scripts/calibration/fit.sh
@@ -3,12 +3,13 @@
 function printhelp { 
     cat <<EOF
 
-usage: $prog [ OPTIONS ] [-d | --data] DATA DIR [DIR ...]
+usage: $prog [ OPTIONS ] [-d DATA | --data DATA] DIR [DIR ...]
 
 options:
  -h/--help  print this message and exits
  -c/--comp  number of components to use (default: "2 3")
  -d/--data  data directory ( *required* )
+ -n/--dry   do not execute commands; print them instead
 
 EOF
 }
@@ -16,7 +17,9 @@ EOF
 
 prog=`basename $0`
 components="2 3"
-temp=`getopt -o hc:d: -l help,comp:data: -- "$@"`
+temp=`getopt -o hc:d:n -l help,comp:data:dry -- "$@"`
+data=''
+dry=0
 
 eval set -- "$temp"
 
@@ -24,12 +27,13 @@ while `true` ; do
     case "$1" in 
         --) shift 1 ; break ;; 
         -h|--help) printhelp ; exit 1 ;; 
-        -c|--comp) components=$1 ; shift 2 ;; 
-        -d|--data) data=$1 ; shift 2 ;;
+        -c|--comp) components="$2" ; shift 2 ;; 
+        -d|--data) data="$2" ; shift 2 ;;
+	-n|--dry) dry=1 ; shift 1 ;;
     esac
 done
 
-if [[ ! -v data ]] ; then 
+if [[ $data = '' ]] ; then 
     printhelp
     exit 1 
 fi
@@ -47,15 +51,19 @@ for d in $dir ; do
         echo "$prog: error: need bounds file"
         exit 1
     fi
-    opts="`<bounds` -p5 -c$c $t -iindex.txt fit -B"
     for c in $components ; do
         for t in -t \  ; do
+	    opts="`<bounds` `<weights_$c` -p5 -c$c $t fit -B"
             if [[ $t = ' ' ]] ; then 
                 simulations="$d"_gmm_"$c".txt 
             else
                 simulations="$d"_gmm_truncated_"$c".txt 
             fi
-            peerstool calibration $opts $data/$d.npy $simulations 
+	    if [[ $dry = 1 ]] ; then
+		echo peerstool calibration $opts $data/$d.npy $simulations 
+	    else
+		peerstool calibration $opts $data/$d.npy $simulations 
+	    fi
         done
     done
     popd &> /dev/null
