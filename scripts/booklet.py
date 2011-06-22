826753cf248bfadb1bbc16210570fd911318597f
ciampagg <ciampagg@usi.ch>
2011-06-22 03:51:25 -0700

    added script for plotting simulations results
    Ignore-this: c86d9f92285258304e46063f5c832f55
    
    darcs-hash:20110622105125-38e5d-55169a6d1734812f4af74e7a06b77023c5b9c450.gz

---
+++ scripts/booklet.py
@@ -0,0 +1,40 @@
+''' saves to pdf kernel density plots of simulation runs '''
+from argparse import ArgumentParser
+import numpy as np
+import re
+
+x = re.compile('\w*?(\d+)')
+key = lambda k : int(x.match(k).groups()[0])
+
+import matplotlib
+matplotlib.use('PDF')
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as pp
+from peers.graphics import kdeplot
+
+parser = ArgumentParser(description=__doc__)
+parser.add_argument('datafile', metavar='data')
+parser.add_argument('outfile', metavar='output')
+parser.add_argument('-every', type=int)
+
+if __name__ == '__main__':
+    ns = parser.parse_args()
+
+    sim = np.load(ns.datafile)
+    pages = PdfPages(ns.outfile)
+    files = sorted(sim.files, key=key)
+    sl = slice(None,None,ns.every)
+    npages = len(files[sl])
+
+    print 'plotting %d pages from %s to %s' % (npages, ns.datafile, ns.outfile)
+
+    try:
+        for i,f in enumerate(files[sl]):
+            kdeplot(sim[f], c='r')
+            pp.xlabel(r'$\tau$ (log-days)')
+            pp.ylabel(r'density')
+            pp.title(f)
+            pages.savefig()
+            print 'page %d of %d: %s' % (i+1, npages, f)
+    finally:
+        pages.close()
