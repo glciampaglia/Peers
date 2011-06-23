545dade45beb5ddf092f23f3e80abb49b0f077fd
ciampagg <ciampagg@usi.ch>
2011-06-23 05:20:13 -0700

    added batch option to scripts/compare.py
    Ignore-this: 761c24020e5cea9a2cfca059a32819a8
    
    darcs-hash:20110623122013-38e5d-65664383ec0fe2a0bd918d0e9d15d1bacbc9b0aa.gz

--- scripts/compare.py
+++ scripts/compare.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 '''
 compares empirical data with simulated density
 '''
@@ -6,20 +7,23 @@ import os.path
 from argparse import ArgumentParser
 import numpy as np
 
-import matplotlib
-matplotlib.use('PDF')
-import matplotlib.pyplot as pp
-
-from peers.graphics import kdeplot
 from peers.utils import sanetext
 
 parser = ArgumentParser(description=__doc__)
 parser.add_argument('datafn', metavar='data')
 parser.add_argument('simufn', metavar='simulation')
-parser.add_argument('outfn', metavar='output')
+parser.add_argument('-output', dest='outfn')
+parser.add_argument('-batch', action='store_true')
 
 if __name__ == '__main__':
     ns = parser.parse_args()
+    if ns.batch:
+        import matplotlib
+        matplotlib.use('PDF')
+
+    import matplotlib.pyplot as pp
+    from peers.graphics import kdeplot
+    
     data = np.load(ns.datafn)
     simu = np.load(ns.simufn)
     dataset = os.path.basename(os.path.splitext(ns.datafn)[0])
@@ -29,5 +33,7 @@ if __name__ == '__main__':
     pp.xlabel(r'$\tau$ (log-days)')
     pp.ylabel(r'density')
     pp.title(sanetext(dataset))
-    pp.savefig(ns.outfn)
-    print 'output written to %s' % ns.outfn
+    if ns.outfn:
+        pp.savefig(ns.outfn, format='pdf')
+        print 'output written to %s' % ns.outfn
+    pp.show()
