493f50bf5603b1c80e2383bb49c675eaf635d84d
ciampagg <ciampagg@usi.ch>
2011-06-26 04:44:27 -0700

    map/reduce implementation for peers.fit.mixreduce
    Ignore-this: 5dc2720131a4962c00cafcf99b15d5fd
    
    Now mixreduce takes two commands: fit, and reduce
    Fitting operates on individual data files and thus can be easily distributed.
    Reduction merges the index with the obtained data file and applies averaging.
    
    Plotting code has been commented and needs to be taken out. booklet script now
    has the same functionality.
    
    darcs-hash:20110626114427-38e5d-0819c68d2e0f6902a2a152c2ef97eeed33d47911.gz

--- peers/fit/reduce.py
+++ peers/fit/reduce.py
@@ -19,92 +19,103 @@ from ..utils import fmt, sanetext
 
 _fields = [ 'mean-%d', 'variance-%d', 'weight-%d' ] 
 
-def main(args):
+def _getfields(n):
     global _fields
-    sniffer = csv.Sniffer()
-    dialect = sniffer.sniff(args.index.read(1000))
-    args.index.seek(0)
-    reader = csv.DictReader(args.index, dialect=dialect)
-    gmmnames = [ [ f % i for i in xrange(args.components) ] for f in _fields ]
-    gmmnames = reduce(list.__add__, gmmnames)
-    outfields = reader.fieldnames[:-1] + gmmnames
-    writer = csv.DictWriter(sys.stdout, outfields, dialect=dialect)
-    writer.writeheader()
-    keyfun = lambda k : tuple([ k[var] for var in reader.fieldnames[:-1] ])
-    data_archive = np.load(args.data)
-    for values, subiter in groupby(reader, keyfun):
-        beta = []
-        for row in subiter:
-            k, _ = os.path.splitext(row['file'])
-            d = data_archive[k]
-            if args.log:
-                d = np.log(d)
-            if len(d):
-                if args.truncated:
-                    model = TGMM(args.components)
-                else:
-                    model = GMM(args.components)
-                model.fit(d, n_iter=args.iterations)
-                means = model.means.ravel()
-                sigmas = np.sqrt(model.covars).ravel()
-                weights = model.weights.ravel()
-                idx = means.argsort()
-                b = np.hstack([means[idx], sigmas[idx], weights[idx]])
-                beta.append(b)
-                if args.plot:
-                    fn, ext = os.path.splitext(row['file'])
-                    fn += '.' + args.format
-                    if args.truncated:
-                        plottruncated(d, model, args.bins, output=fn)
-                    else:
-                        plot(d, model, args.bins, output=fn, **params)
-            else:
-                warn('no data: %s' % row['file'], category=UserWarning)
-        if len(beta) == 0:
-            warn('skipping: %s.' % ', '.join(values), category=UserWarning)
-            continue
-        beta = np.asarray(beta).mean(axis=0)
-        outrow = dict(row)
-        del outrow['file']
-        outrow.update(zip(gmmnames, beta))
-        writer.writerow(outrow)
-        sys.stdout.flush()
+    return reduce(list.__add__, [[ f % i for f in _fields ] for i in xrange(n)])
 
-def plot(data, model, bins=10, output=None, **params):
-    '''
-    plots histogram of log-lifetime data with GMM fit (as stacked densities) and
-    save output to file 
-    '''
-    fig = pp.figure()
+def _reduce(args):
+    reader = csv.reader(args.indexfile, delimiter=',', quoting=0)
+    params = reader.next()
+    N = len(params) - 1
+    index = dict([ (row[-1], map(float, row[:-1])) for row in reader ])
+    reader = csv.reader(args.datafile, delimiter=',', quoting=0)
+    for row in reader:
+        key = row[0]
+        value = map(float, row[1:])
+        if key in index:
+            index[key] += value
+        else:
+            raise ValueError('not in index: %s' % key)
+    arr = np.asarray(index.values())
+    idx = arr[:,:N].argsort(axis=0)[:,0]
+    arr = arr[idx]
+    C = arr.shape[1] - N
+    fields = params[:-1] + _getfields(C/3)
+    writer = csv.DictWriter(sys.stdout, fields, delimiter=',', quoting=0)
+    kf = lambda k : tuple(k[:N])
+    writer.writeheader()
+    for key, subiter in groupby(arr, kf):
+        row = dict(zip(fields, key))
+        x = np.asarray([ r[N:] for r in subiter ])
+        row.update(zip(fields[N:], x.mean(axis=0)))
+        writer.writerow(row)
+
+def _fit(args):
+    data = np.load(args.datafile)
+    if args.log:
+        data = np.log(data)
+    if len(data) == 0:
+        raise ValueError('empty data file: %s' % args.datafile)
+    if args.truncated:
+        model = TGMM(args.components)
+    else:
+        model = GMM(args.components)
+    model.fit(data, n_iter=args.iterations)
     means = model.means.ravel()
-    variances = np.asarray(model.covars).ravel()
-    RV = map(norm, means, variances)
-    mixturehist(data, RV, model.weights, figure=fig)
-    pp.xlabel(r'$u = \mathrm{log}(\tau)$ (days)')
-    pp.ylabel(r'Prob. Density $p(x)$')
-    if len(params):
-        title = ', '.join(map(lambda k : ' = '.join(k), params.items()))
-        pp.title(sanetext(title), fontsize='small')
-    elif output is not None:
-        pp.title(sanetext(output.name), fontsize='small')
-    pp.draw()
-    if output is not None:
-        pp.savefig(output, format=fmt(output, 'pdf'))
-    pp.show()
+    sigmas = np.sqrt(model.covars).ravel()
+    weights = model.weights.ravel()
+    idx = means.argsort()
+    beta = [means[idx], sigmas[idx], weights[idx]]
+    beta = (args.datafile,) + reduce(tuple.__add__, map(tuple, beta))
+    print ','.join(map(str,beta))
+
+#def plot(data, model, bins=10, output=None, **params):
+#    '''
+#    plots histogram of log-lifetime data with GMM fit (as stacked densities) and
+#    save output to file 
+#    '''
+#    fig = pp.figure()
+#    means = model.means.ravel()
+#    variances = np.asarray(model.covars).ravel()
+#    RV = map(norm, means, variances)
+#    mixturehist(data, RV, model.weights, figure=fig)
+#    pp.xlabel(r'$u = \mathrm{log}(\tau)$ (days)')
+#    pp.ylabel(r'Prob. Density $p(x)$')
+#    if len(params):
+#        title = ', '.join(map(lambda k : ' = '.join(k), params.items()))
+#        pp.title(sanetext(title), fontsize='small')
+#    elif output is not None:
+#        pp.title(sanetext(output.name), fontsize='small')
+#    pp.draw()
+#    if output is not None:
+#        pp.savefig(output, format=fmt(output, 'pdf'))
+#    pp.show()
+
+def main(args):
+    args.func(args)
 
 def make_parser():
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument('index', type=FileType('r'), help='index file')
-    parser.add_argument('data', help='data file (NPZ)', type=FileType('r'))
-    parser.add_argument('-c', '--components', type=int, default=2)
-    parser.add_argument('-p', '--plot', action='store_true', 
-            help='stacked area plot')
-    parser.add_argument('-t', '--truncated', action='store_true')
-    parser.add_argument('-i', '--iterations', type=int, default=100)
-    parser.add_argument('-l', '--log', action='store_true', help='fit log-data')
-    parser.add_argument('-b', '--bins', type=int, metavar='NUM', default=10)
-    parser.add_argument('-f' ,'--format', help='graphic output format (default:'
-            ' %(default)s)', default='pdf')
+    subparsers = parser.add_subparsers()
+    fitparser = subparsers.add_parser('fit', help='mixture parameters estimate')
+    fitparser.add_argument('datafile', metavar='data', help='NumPy binary array file')
+    fitparser.add_argument('-c', '--components', type=int, default=2, 
+            help='default: %(default)d')
+    fitparser.add_argument('-t', '--truncated', action='store_true')
+    fitparser.add_argument('-i', '--iterations', type=int, default=1000, 
+            help='EM iterations (default: %(default)d')
+    fitparser.add_argument('-l', '--log', action='store_true', help='use log-data')
+    fitparser.set_defaults(func=_fit)
+    redparser = subparsers.add_parser('reduce', help='reduction by averaging')
+    redparser.add_argument('indexfile', metavar='index', type=FileType('r'))
+    redparser.add_argument('datafile', metavar='data', help='fit results',
+            type=FileType('r'))
+    redparser.set_defaults(func=_reduce)
+#    parser.add_argument('-p', '--plot', action='store_true', 
+#            help='stacked area plot')
+#    parser.add_argument('-b', '--bins', type=int, metavar='NUM', default=10)
+#    parser.add_argument('-f' ,'--format', help='graphic output format (default:'
+#            ' %(default)s)', default='pdf')
     return parser
 
 if __name__ == '__main__':
